1d3297e1cee79e876c347fe43f09935ff86542bd
Wander Bravo <wander.bravo@gmail.com>
2015-08-25 23:55:03 -0500

    adding more changes

--- callbacksample/app/models/order.rb
+++ callbacksample/app/models/order.rb
@@ -3,9 +3,17 @@ class Order < ActiveRecord::Base
 
   validates :total_price, numericality: true
 
-  before_create :apply_discount, if: lambda { |order| order.discount? }
+  after_create :apply_discount, if: lambda { |order| order.discount? }
 
   def apply_discount
+    Rails.logger.debug "about to apply discount"
     self.total_price = self.total_price * 0.95
+    Rails.logger.debug "discount applied"
+  end
+
+  Order.transaction do
+    Order.create
+    sleep 10
+    raise ActiveRecord::Rollback
   end
 end
